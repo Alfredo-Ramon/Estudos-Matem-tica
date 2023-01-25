54fa6c63dbc12933e658143703199e769fa35c65
Alfredo-Ramon <alfredoramon354@gmail.com>
2023-01-25 16:45:31 -0300

    Removido cor(ERRO Auto-Py-to-Exe), Inserido ExpressõesRegulares

--- Funções/Interface/__init__.py
+++ Funções/Interface/__init__.py
@@ -1,9 +1,9 @@
 class Menus:
     def MenuBemVindo():
-        print('\33[35m_\33[m'*100)
-        print(F'\33[41m{"BEM VINDO AO APLICATIVO: Respota Rápida".center(100)}\33[m')
+        print('_'*100)
+        print(F'{"BEM VINDO AO APLICATIVO: Respota Rápida".center(100)}')
         print(f'{"Use a vontade :)".center(100)}')
-        print('\33[35m_\33[m'*100)
+        print('_'*100)
     
     def MenuEscolhas():
         print(f'[1]: Adição')
@@ -11,12 +11,12 @@ class Menus:
         print(f'[3]: Multiplicação')
         print(f'[4]: Divisão ')
         print(f'[5]: Finalizar')
-        print('\33[35m_\33[m'*100)
+        print('_'*100)
     
         
     def Escolha(numero = 0):
         if numero == 0:
-            print(f'\33[31mPor favor escolha umas das opções [1,2,3,4,5]\33[m')
+            print(f'Por favor escolha umas das opções [1,2,3,4,5]')
         
         
         
--- main.py
+++ main.py
@@ -3,3 +3,18 @@ import re
 
 Interface.Menus.MenuBemVindo()
 Interface.Menus.MenuEscolhas()
+
+
+while True:
+    while True:
+        escolha = str(input('Digite uma opção: '))
+        if re.fullmatch(re.compile('[1-5]'),escolha) == None:
+            print('Erro')
+        else:
+            print('OK')
+            break
+    if escolha == '5' :
+        break
+             
+    
+Interface.Menus.Escolha(escolha)
