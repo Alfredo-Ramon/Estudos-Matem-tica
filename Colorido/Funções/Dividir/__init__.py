aa9d38d81a34aa4ac854010eab06c87554cea375
Alfredo-Ramon <alfredoramon354@gmail.com>
2023-01-28 18:01:38 -0300

    Inicio Código em cores

---
+++ Colorido/Funções/Dividir/__init__.py
@@ -0,0 +1,98 @@
+def MenuDivisão():
+    print()
+    print()
+    print(F'{"Divisão".center(100)}')
+    print('_'*100)
+    print(F'{"Escolha uma dificuldade".center(100)}')
+    print('_'*100)
+    
+def MenuDivisãoEscolha():
+    print(f'[1]: Fácil')
+    print(f'[2]: Finalizar')
+    print('_'*100)
+    
+def DivisãoFacil():
+    import random
+    erros = 0
+    print()
+    print()
+    print(F'{"Divisão".center(100)}')
+    print('_'*100)
+    print(F'{"Facil: O programa ira fazer 10 contas todas contendo só uma unidade".center(100)}')
+    print('_'*100)
+    print()
+    print()
+    for Conta in range(10):
+            print('*'*100)
+            while True :
+                while True:
+                    Numero1 = random.randint(1,9)
+                    Numero2 = random.randint(1,Numero1)
+                    conta = Numero1/Numero2
+                    conta = str(conta)
+                    if len(conta) > 4:
+                        print('Mudo maior que 4')
+                        print(conta)
+                        Numero1 = random.randint(1,9)
+                        Numero2 = random.randint(1,Numero1) 
+                    else:
+                        break
+                if Numero1 == Numero2:
+                    print('Mudo igual ')
+                    Numero1 = random.randint(1,9)
+                    Numero2 = random.randint(1,Numero1)
+                elif Numero2 == 1:
+                    print('Mudo igual 1')
+                    Numero2 = random.randint(1,Numero1)
+                else:
+                    if Conta == 0:
+                        UltimaContaNumero1 = Numero1
+                        UltimaContaNumero2 = Numero2
+                        break
+                    elif Conta > 0:
+                        if UltimaContaNumero1 != Numero1 and UltimaContaNumero2 != Numero2 :    
+                            break
+                        if UltimaContaNumero1 == Numero1 and UltimaContaNumero2 == Numero2 :    
+                            print(f'{Numero1}/{Numero2} trocou')
+                            
+                            
+            
+            print(f'{Conta+1}: {Numero1}/{Numero2} = ?') 
+            while True:
+                try:
+                    resposta = float(input('Resposta: ')) 
+                except :
+                    print('#'*100)
+                    print(f'{Numero1}/{Numero2} = ?'.center(100))
+                    print(f'{"Sixtaxe : Resposta com valor Inteiro X".center(100)} ')
+                    print(f'{"Sixtaxe : Resposta com valor Decimal X.XX".center(100)} ')  
+                    print(f'{"Por favor use só números [1|2|3|4|5|6|7|8|9|0]".center(100)}')
+                    print(f'{"User . no lugar de ,".center(100)}')
+                    print('#'*100)
+                else:
+                    if resposta == Numero1/Numero2:
+                        print('Acertou')
+                        print('*'*100)
+                        print()
+                        print()
+                        break
+                    else:
+                        if resposta < Numero1/Numero2:
+                            erros += 1
+                            print()
+                            print('#'*100)
+                            print(f'{Numero1}/{Numero2} = ?'.center(100)) 
+                            print('Mais'.center(100))
+                            print('#'*100)
+                        elif resposta > Numero1/Numero2:
+                            erros += 1
+                            print()
+                            print('#'*100)
+                            print(f'{Numero1}/{Numero2} = ?'.center(100)) 
+                            print('Menos'.center(100))
+                            print('#'*100)
+    print(f'Você errou: {erros} vezes')
+    
+
+if __name__ == '__main__':
+    DivisãoFacil()
---
+++ Colorido/Funções/Interface/__init__.py
@@ -0,0 +1,31 @@
+class Menus:
+    def MenuPrincipalBemVindo():
+        print('#'*100)
+        print(F'{"BEM VINDO AO APLICATIVO: Resposta Rápida".center(100)}')
+        print('#'*100)
+    
+    def MenuPrincipalEscolhas():
+        print()
+        print()
+        print(f'{"Menu Principal".center(100)}')
+        print('-'*100)
+        print(f'{"Escolha uma opção".center(100)}')
+        print('-'*100)
+        print(f'[1]: Adição')
+        print(f'[2]: Subtração')
+        print(f'[3]: Multiplicação')
+        print(f'[4]: Divisão ')
+        print(f'[5]: Finalizar')
+        print('_'*100)
+    
+        
+
+        
+        
+        
+
+if __name__ == '__main__' :
+    Menus.MenuPrincipalBemVindo()
+    Menus.MenuPrincipalEscolhas()
+
+        
